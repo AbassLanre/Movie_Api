14c2c82aab29f7b6897498a6aebe199decb9f7c5
Abass <kalanruse@gmail.com>
2020-12-15 01:17:03 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+var apiKey ='http://www.omdbapi.com/?i=tt3896198&apikey=11b413da';
+void main() {
+  runApp(MaterialApp(
+    title: 'Movies Api',
+    // home: Home(),
+  ));
+}
